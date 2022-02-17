86655eb4195a9f6609c24c8b34688549dbb49684
user <user@PC421-04>
2022-02-17 16:06:21 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_02_22
+{
+    class Program
+    {
+        class Pytanie
+        {
+            public string tresc;
+            public bool cecha;
+            public Pytanie tak, nie; 
+            
+            public Pytanie(string _tresc, bool _cecha)
+            {
+                tresc = _tresc;
+                cecha = _cecha;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //zmienne
+
+            //funkcje
+        }
+    }
+}
+    
